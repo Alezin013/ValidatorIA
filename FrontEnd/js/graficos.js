3c520e4b5e05a700766864d1e8880286563331ea
Fmbirl <pippodigiuseppe19@gmail.com>
2025-10-08 21:15:46 -0300

    mexendo-html-css

---
+++ FrontEnd/js/graficos.js
@@ -0,0 +1,44 @@
+const ctx = document.getElementById('grafico-pizza');
+
+new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [30, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+// GRAFICO PIZZA VAMOS COLOCAR A PORCENTAGEM DE QUANTO FOI USADO DE CADA LINGUAGEM
+// GRAFICO POLAR VAMOS COLOCAR A NOTA EM CADA LINGUAGEM
+
+const ctd = document.getElementById('grafico-polar');
+
+new Chart(ctd, {
+  type: 'polarArea',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [1, 10, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
